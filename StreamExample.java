066e1acff5f5c6d6a701305bcf52cc9a01cfaa79
navpreet7 <e-kawakami@KYC-MacBookAir001.local>
2019-12-12 10:51:11 +0900

    stream example added

---
+++ StreamExample.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Product{  
+    int id;  
+    String name;  
+    float price;  
+    public Product(int id, String name, float price) {  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+} 
+public class StreamExample {
+	public static void main(String[] args) {
+		List<Product> productsList = new ArrayList<Product>();  
+        //Adding Products  
+        productsList.add(new Product(1,"HP Laptop",25000f));  
+        productsList.add(new Product(2,"Dell Laptop",30000f));  
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
+        productsList.add(new Product(4,"Sony Laptop",28000f));  
+        productsList.add(new Product(5,"Apple Laptop",90000f)); 
+        
+        productsList.stream().filter(p->p.price>20000).map(map->map.price).
+        forEach(System.out::println);
+	}
+
+}
