4066649954bbc16891a208bd04955552e3083f65
Navpreet Singh <nav.mehta8@gmail.com>
2023-01-31 17:55:49 -0500

    updated class

--- CollectorsExample.java
+++ CollectorsExample.java
@@ -5,33 +5,35 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-class Data{
+class Data {
 	String name;
 	int price;
+
 	public Data(String name, int price) {
 		super();
 		this.name = name;
 		this.price = price;
 	}
-	
+
 }
+
 public class CollectorsExample {
 	public static void main(String[] args) {
 		List<Data> list = new ArrayList<Data>();
-		list.add(new Data("apple",100));
-		list.add(new Data("pineapple",400));
-		list.add(new Data("kiwi",300));
-		list.add(new Data("guava",200));
-		
-		//Fetching data as list
-		List<Integer> priceList = list.stream().map(x->x.price).collect(Collectors.toList());
+		list.add(new Data("apple", 100));
+		list.add(new Data("pineapple", 400));
+		list.add(new Data("kiwi", 300));
+		list.add(new Data("guava", 200));
+		list.add(new Data("banana", 200));
+		// Fetching data as list
+		List<Integer> priceList = list.stream().map(x -> x.price).collect(Collectors.toList());
 		System.out.println(priceList.toString());
-		
-		//Fetching data as set
-		Set<Integer> priceSet = list.stream().map(x->x.price).collect(Collectors.toSet());
+
+		// Fetching data as set
+		Set<Integer> priceSet = list.stream().map(x -> x.price).collect(Collectors.toSet());
 		System.out.println(priceSet.toString());
-		
-		int sum = list.stream().collect(Collectors.summingInt(x->x.price));
-		System.out.println(sum);		
+
+		int sum = list.stream().collect(Collectors.summingInt(x -> x.price));
+		System.out.println(sum);
 	}
 }
